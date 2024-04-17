da0ddaab9998a71d5a170cd778c0b96484708fef
kennethbchen <kennethbchen@gmail.com>
2024-04-17 12:06:21 -0400

    Added sample bot code

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import json
+import discord
 
 config = json.load(open('config.json'))
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Lgged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(config["discord_bot_token"])
