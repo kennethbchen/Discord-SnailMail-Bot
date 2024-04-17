a81101b1fba8370dbc3f26dae488c09cda1beb8c
kennethbchen <kennethbchen@gmail.com>
2024-04-17 18:26:27 -0400

    Stopped using discord.ext.commands package

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 import json
 import discord
-from discord.ext import commands
+from discord import app_commands
 
 config = json.load(open('config.json'))
 
-intents = discord.Intents.default()
-intents.message_content = True
+# https://github.com/Rapptz/discord.py/blob/v2.3.2/examples/app_commands/basic.py
+class MyClient(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        self.tree = app_commands.CommandTree(self)
 
-bot = commands.Bot(command_prefix='$', intents=intents, client=discord.Client(intents=intents))
+    async def setup_hook(self):
 
-@bot.event
+        if config["debug"]["enabled"]:
+            print("Copying commands to debug guild...")
+            self.tree.copy_global_to(guild=discord.Object(id=config["debug"]["debug_guild_id"]))
+
+        print("Syncing Commands...")
+        await self.tree.sync()
+
+client = MyClient()
+
+@client.event
 async def on_ready():
+    print(f'Logged in as {client.user}')
 
-    print("Syncing bot commands...")
-    commands = await bot.tree.sync()
-    print("Synced Commands:", commands)
+@client.tree.command()
+async def mailbox(interaction: discord.Interaction):
+    await interaction.response.send_message("mailbox")
 
-    print(f'Logged in as {bot.user}')
+@client.tree.command()
+async def register(interaction: discord.Interaction):
+    await interaction.response.send_message("register")
 
-@bot.hybrid_command()
-async def test(ctx):
-    await ctx.send("test command")
+@client.tree.command()
+async def link(interaction: discord.Interaction):
+    await interaction.response.send_message("link")
 
-bot.run(config["discord_bot_token"])
+client.run(config["discord_bot_token"])
